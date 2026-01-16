c7f636d5327c18817626e1a633737361ab1ac749
Iván Lahuerta <dinkelo@gmail.com>
2026-01-16 15:25:18 +0100

    chore: bump version to 0.2.1

--- live/__init__.py
+++ live/__init__.py
@@ -20,7 +20,7 @@ Usage:
     python -m live.multi_monitor
 """
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 # Expose main classes for easy imports
 from .connector import MT5Connector, AccountInfo, AccountType
