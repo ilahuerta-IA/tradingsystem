9cb0f2861bd6ecfbf75bddc3844a554722e16076
Iván Lahuerta <dinkelo@gmail.com>
2026-01-27 21:48:52 +0100

    chore: bump version to 0.3.1 (TRADE_CLOSED logging)

--- live/__init__.py
+++ live/__init__.py
@@ -20,7 +20,7 @@ Usage:
     python -m live.multi_monitor
 """
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 # Expose main classes for easy imports
 from .connector import MT5Connector, AccountInfo, AccountType
